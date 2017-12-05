ab465aee9d689f0b01c9b7fa0ce3dd2f4df89297
Henry J Schmale <hschmale16@gmail.com>
2017-12-04 21:20:54 -0500

    Do Codes for Day 25 Part 1 2015

--- 2015/25/codes.py
+++ 2015/25/codes.py
@@ -4,11 +4,22 @@ t_col = 3019
 multiply = 252533
 modulus = 33554393
 
-def code(x):
-    code.last = x * multiply % modulus
+def code():
+    code.last = code.last * multiply % modulus
     return code.last
-code.last = start
+code.last = start 
 
-row = 1
-current = (0,0)
-n = 0 
+
+max_row = 2
+row, col = (2, 1)
+
+while True:
+    print(row,col,code())
+    if row == t_row and col == t_col:
+        break
+    row -= 1
+    col += 1
+    if row == 0:
+        max_row += 1
+        row = max_row
+        col = 1 

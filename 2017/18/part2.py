4dbc1b5d0e02512e3548b3f79f27711e3ad9fa35
Henry J Schmale <hschmale16@gmail.com>
2017-12-19 16:54:20 -0500

    Finish Day 18

--- 2017/18/part2.py
+++ 2017/18/part2.py
@@ -1,49 +1,70 @@
 import sys
 from collections import Counter
-
+from collections import deque
 
 def try_int(x):
-    try: return int(x.replace(',', ''))
-    except: return x.replace(',', '')
-
-regs = Counter()
-cmds = []
+    try:
+        return int(x.replace(',', ''))
+    except ValueError:
+        return x.replace(',', '')
 
 with open(sys.argv[1]) as f:
-    cmds = [tuple(map(try_int, line.split())) for line in f.readlines()]
-print(cmds)
-progcnter = 0
+    CMDS = [tuple(map(try_int, line.split())) for line in f.readlines()]
 
-def get_value(target):
-    if type(target) is int:
-        return target
-    return regs[target]
+class DuetRunner:
+    def __init__(self, r, s, p):
+        assert r is not s
+        self.progcnter = 0
+        self.r_queue = r
+        self.s_queue = s
+        self.regs = Counter({'p': p})
+        self.waiting = False
+        self.snd_count = 0
+    
+    def get_value(self, target):
+        if type(target) is int:
+            return target
+        return self.regs[target]
 
-try:
-    last = None
-    while True:
-        print(regs, progcnter)
-        cmd = cmds[progcnter]
+    def run(self):
+        cmd = CMDS[self.progcnter]
         ins = cmd[0]
         if ins == 'snd':
-            last = get_value(cmd[1])
-            print(last)
+            self.s_queue.append(self.get_value(cmd[1]))
+            self.snd_count += 1
         elif ins == 'set':
-            regs[cmd[1]] = get_value(cmd[2])
+            self.regs[cmd[1]] = self.get_value(cmd[2])
         elif ins == 'add':
-            regs[cmd[1]] += get_value(cmd[2])
+            self.regs[cmd[1]] += self.get_value(cmd[2])
         elif ins == 'mul':
-            regs[cmd[1]] *= get_value(cmd[2])
+            self.regs[cmd[1]] *= self.get_value(cmd[2])
         elif ins == 'mod':
-            regs[cmd[1]] %= get_value(cmd[2])
+            self.regs[cmd[1]] %= self.get_value(cmd[2])
         elif ins == 'rcv':
-            if get_value(cmd[1]) != 0:
-                print(last)
-                break
+            if len(self.r_queue) == 0:
+                self.waiting = True
+                return
+            else:
+                self.waiting = False
+                self.regs[cmd[1]] = self.r_queue.popleft()
         elif ins == 'jgz':
-            if get_value(cmd[1]) > 0:
-                progcnter += get_value(cmd[2])
-                continue
-        progcnter += 1
-except:
-    print(regs)
+            if self.get_value(cmd[1]) > 0:
+                self.progcnter += self.get_value(cmd[2])
+                return
+        self.progcnter += 1
+
+prog0_q = deque()
+prog1_q = deque()
+
+prog0 = DuetRunner(prog0_q, prog1_q, 0)
+prog1 = DuetRunner(prog1_q, prog0_q, 1)
+
+while True:
+    #print(len(prog0_q), len(prog1_q))
+    prog0.run()
+    prog1.run()
+    if prog0.waiting and prog1.waiting:
+        break
+
+print(prog0.snd_count)
+print(prog1.snd_count)

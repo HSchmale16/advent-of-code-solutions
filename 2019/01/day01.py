06d2cffaf59dc306f0b45a1225c5f9e83b20ec04
Henry J Schmale <hschmale16@gmail.com>
2019-12-01 22:50:53 -0500

    Update day01.py

--- 2019/01/day01.py
+++ 2019/01/day01.py
@@ -1,17 +1,19 @@
 import sys
 import math
 
+def fuel(x):
+    return math.trunc(x / 3) - 2
+
 counter1 = 0
 counter2 = 0
 for line in sys.stdin.readlines():
     x = int(line)
-    fuel1 = math.trunc(x / 3) - 2
+    fuel1 = fuel(x)
     counter1 += fuel1
     fuel2 = fuel1
     while fuel1 > 0:
-        fuel1 = math.trunc(fuel1 / 3) - 2
-        if fuel1 > 0:
-            fuel2 += fuel1
+        fuel1 = fuel(fuel1)
+        fuel2 += fuel1 if fuel1 > 0 else 0
     counter2 += fuel2
 
 print(counter1)

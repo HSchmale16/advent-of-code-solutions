3b45c3cff49873d403e49fd8824a4e5e692ed07b
Henry J Schmale <hschmale16@gmail.com>
2022-12-11 12:13:48 -0500

    day 10 part 1

---
+++ 2022/10/day10.py
@@ -0,0 +1,28 @@
+import sys
+
+targets = [20, 60, 100, 140, 180, 220]
+cycle = 0
+x = 1
+pc = 1
+strength = 0
+
+cmds = [l.split() for l in sys.stdin.readlines()]
+
+def proc_cycle():
+    global strength
+    if pc in targets:
+        strength += pc * x
+        print(x, pc, strength)
+
+for c in cmds:
+    if len(c) == 1:
+        pc += 1
+        proc_cycle()
+    else:
+        v = int(c[1])
+        pc += 1
+        proc_cycle()
+        pc += 1
+        x += v
+        proc_cycle()
+
